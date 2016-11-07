91d85fb5e36ef0ca0d8b1c9a32db7f6cf3fe26e1
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-07 15:20:22 -0500

    I tested the sum function in calc.js and figured out that the forEach was not returning a total

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -8,9 +8,27 @@
 
     describe('my calc', function() {
       it('should know that obviously one equals one', function () {
-        expect(1).to.equal(1);        
+        expect(1).to.equal(1);
       });
+
+      describe('sum', function() {
+        beforeEach(function() {
+          console.log('Am I before?');
+        });
+
+      it('should have a function called sum', function() {
+        expect(window.calc.sum).to.be.a('function');
+      });
+
+      it('should give a total when given a number', function() {
+        var result = window.calc.sum([2,3,5]);
+        console.log(window.calc.sum);
+        expect(result).to.equal(10);
+      })
+
+      })
     })
+
 
 
 
